8fe0cb605da0bf687bfb174ef3dac6ff046f05ca
Satyam <satyamch2710@gmail.com>
2023-12-06 12:45:25 +0530

    Initial commit

---
+++ music-list.js
@@ -0,0 +1,62 @@
+let allMusic = [
+    {
+      name: "Yaari",
+      artist: "Nikk",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Chand Sifarish",
+      artist: "Kailash kher and Shaan",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Saiyaara",
+      artist: "Mohit Chauhan ",
+      img: "saiyaara.png",
+      src: "music-3"
+    },
+    {
+      name: "Hue Bechain",
+      artist: "Yasser Desai",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "Punjabiyan Di Dhee",
+      artist: "Guru Randhawa",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+      name: "Ve Kamleya",
+      artist: "Arijit Singh and Shreya Ghosal",
+      img: "music-6",
+      src: "music-6"
+    },
+    {
+      name: "Rista Rista",
+      artist: "Stebin Ben",
+      img: "music-7",
+      src: "music-7"
+    },
+    {
+      name: "Ram Siya Ram",
+      artist: "Sachet and Parampara ",
+      img: "music-8",
+      src: "music-8"
+    },
+    {
+      name: "Judaiyaan",
+      artist: "Rajat Nagpal",
+      img: "music-9",
+      src: "music-9"
+    },
+    {
+      name: "Lambiyan Si Judaiyaan",
+      artist: "Arijit Singh",
+      img: "music-10",
+      src: "music-10"
+    },
+  ];
